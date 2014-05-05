3531a7203f0dfd9b94fd893ef308817fd4105216
Erlis Vidal <erlis.vidal@gmail.com>
2014-05-05 10:56:10 -0400

    adding more solutions

--- answers/src/answers/core.clj
+++ answers/src/answers/core.clj
@@ -13,4 +13,6 @@
   (show elementary/nothing-but-the-truth true)
   (show elementary/simple-math 4)
   (show elementary/intro-to-string "HELLO WORLD")
-  (show elementary/intro-to-lists :a :b :c))
+  (show elementary/intro-to-lists :a :b :c)
+  (show elementary/lists-conj '(1 2 3 4) )
+  (show elementary/intro-to-vectors :a :b :c))
--- answers/src/answers/elementary.clj
+++ answers/src/answers/elementary.clj
@@ -1,11 +1,29 @@
 (ns answers.elementary
-  (:use [clojure.test]))
+  (:use [clojure.test :only (is)]))
 
-(defn intro-to-lists [& __]
+
+(defn intro-to-vectors
+  "Vectors can be constructed several ways. You can compare them with lists."
+  [& __]
+  (is (= (apply vector __) (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))) )
+
+
+(defn lists-conj
+  "When operating on a list, the conj function will return a new list with one or more items  'added' to the front."
+  [__]
+  (is (= __ (conj '(2 3 4) 1)))
+  (is (= __ (conj '(3 4) 2 1))))
+
+
+(defn intro-to-lists
+  "Lists can be constructed with either a function or a quoted form."
+  [& __]
   (is (= (apply list __) '(:a :b :c))))
 
 
-(defn intro-to-string [__]
+(defn intro-to-string
+  "Clojure strings are Java strings. This means that you can use any of the Java string methods on Clojure strings."
+  [__]
   (is (= __ (.toUpperCase "hello world"))))
 
 
