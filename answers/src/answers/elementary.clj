2f5ecf8d6ee7ed4d087e57afe9e94cdc49c35216
Erlis Vidal <erlis.vidal@gmail.com>
2014-05-05 16:19:50 -0400

    ADDED: more solutions

--- answers/src/answers/elementary.clj
+++ answers/src/answers/elementary.clj
@@ -1,6 +1,34 @@
 (ns answers.elementary
   (:use [clojure.test :only (is)]))
 
+
+(defn intro-to-seqs
+  "All Clojure collections support sequencing. You can operate on sequences with functions like first, second, and last."
+  [__]
+  (is (= __ (first '(3 2 1))))
+  (is (= __ (second [2 3 4])))
+  (is (= __ (last (list 1 2 3)))))
+
+
+(defn maps-conj
+  "When operating on a map, the conj function returns a new map with one or more key-value pairs 'added'"
+  [__]
+  (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3])))
+
+
+(defn intro-to-maps
+  "Maps store key-value pairs. Both maps and keywords can be used as lookup functions. Commas can be used to make maps more readable, but they are not required."
+  [__]
+  (is (= __ ((hash-map :a 10, :b 20, :c 30) :b)))
+  (is (= __ (:b {:a 10, :b 20, :c 30}))))
+
+
+(defn sets-conj
+  "When operating on a set, the conj function returns a new set with one or more keys 'added'."
+  [__]
+  (is (= #{1 2 3 4} (conj #{1 4 3} __))))
+
+
 (defn intro-to-sets
   "Sets are collections of unique values."
   [__]
