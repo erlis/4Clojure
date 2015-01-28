80a02d7c099b9f52d30c684264680ac26d5ac7b8
Erlis Vidal <erlis@erlisvidal.com>
2015-01-28 08:03:41 -0500

    ADDED: #69

--- answers/src/answers/medium.clj
+++ answers/src/answers/medium.clj
@@ -11,12 +11,17 @@
   (is (= (__ - {1 10, 2 20} {1 3, 2 10, 3 15})
          {1 7, 2 10, 3 15}))
   (is (= (__ concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
-         {:a [3 4 5], :b [6 7], :c [8 9]}))
-  )
+         {:a [3 4 5], :b [6 7], :c [8 9]})))
 
 (defn merge-with-a-function-answer
-  [f & colls]
-  )
+  [f & maps]
+  (reduce (fn [m1 m2]
+            (reduce #(let [_acc %1
+                           _key (first %2)
+                           _val (last %2)]
+                       (conj _acc [_key (if (contains? _acc _key)
+                                        (f (_acc _key) _val)
+                                        _val)])) m1 m2)) maps))
 
 
 (defn prime-numbers
