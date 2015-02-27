67d008096bc3e09e6218e0318edf882ebc9d49bb
Erlis Vidal <erlis.vidal@gmail.com>
2015-02-27 16:53:23 -0500

    ADDED: ex 70 answer

--- answers/src/answers/medium.clj
+++ answers/src/answers/medium.clj
@@ -14,7 +14,8 @@
   )
 
 (defn word-sorting-answer
-  [])
+  [s]
+  (sort-by #(.toUpperCase %) (clojure.string/split s #"[^\w+]")))
 
 (defn merge-with-a-function
   "Write a function which takes a function f and a variable number of maps. Your function should return a map that consists of the rest of the maps conj-ed onto the first. If a key occurs in more than one map, the mapping(s) from the latter (left-to-right) should be combined with the mapping in the result by calling (f val-in-result val-in-latter)
